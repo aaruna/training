540f9c4ea13812bcff56cbcc71cbef545ceb9a52
Aaruna Godthi <aaruna@tuplejump.com>
2016-02-02 22:50:10 +0530

    Solution for Bloxorz assignment

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -62,6 +62,19 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("newNeighboursOnly") {
+    new Level1 {
+      assert(this.newNeighborsOnly(Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      ) ==  Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream)
+    }
+  }
+
 
 	test("optimal solution for level 1") {
     new Level1 {
